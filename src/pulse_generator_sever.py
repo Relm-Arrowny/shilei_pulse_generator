a105e609441c56550fb39819315651bfcb1ca8c2
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-07-03 17:13:31 +0100

    add command option

--- src/pulse_generator_sever.py
+++ src/pulse_generator_sever.py
@@ -3,7 +3,7 @@ Created on 01 july 2025
 
 @author: wvx67826
 '''
-
+from serial import Serial
 
 import socket
 import numpy as np
@@ -12,17 +12,18 @@ from usbserial import USBSerial
 
 class Pulse_G_Sever():
     
-    def __init__(self, ip ="", port = 8888):
+    def __init__(self, ip ="", port = 8888, usb_port = "COM4"):
         self.HOST = ip # The server's hostname or IP address
         self.PORT = port  # The port used by the server
         self.serverRunning = False
         self.conn = None
         self.daq = None
         self.device = None
+        self.usb_port = usb_port
         
     def start(self):
         self.serverRunning = True
-        self.device = USBSerial(port = 'COM4', baudrate = 9600, timeout = 3, open = True)
+        self.device = USBSerial(port = self.usb_port, baudrate = 9600, timeout = 3, open = True)
         self.device.write("AT\r\n")
         print(f"usb connection: {self.device.readline()}")
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -58,33 +59,43 @@ class Pulse_G_Sever():
             return data
         else:
             return "Fail to read"        
+    def pass_command(self, value):
+        self.device.write(f"{value}\r\n")
+        data = ""
+        while True:
+            new_data =  self.device.readline()
+            data = data + new_data + " "
+            if new_data =="":
+                break
+
+        return data
             
     def processRequest(self, s):
         print("waiting for command")
-        queued_data = self.conn.recv(1024).splitlines()
+        queued_data = self.conn.recv(1024).decode("utf-8").splitlines()
         print(queued_data)
         
         if not queued_data:
-            queued_data =  [b"close"]
+            queued_data =  ["close"]
             
         for data in queued_data:
             
             data = data.split()   
-            if data[0] == b"stopSever":
+            if data[0] == "stopSever":
                 self.serverRunning = False
-                self.conn.sendall(b"server stopping")
+                self.conn.sendall("server stopping")
                 print("Server stopping")
                 self.conn.close()
                 self.daq.disconnect()
                 
-            elif data[0] == b"check_status":
+            elif data[0] == "check_status":
                 try:
                     self.device.write("AT\r\n")
                     send_data = self.device.readline()
                     self.conn.sendall(send_data.encode("utf_8"))
                 except Exception as e:
                     self.sendError("data read failed: %s" %e)
-            elif data[0] == b"set_delay":
+            elif data[0] == "set_delay":
                 if len(data)>1:
                     value = float (data[1])
                 try:
@@ -93,9 +104,17 @@ class Pulse_G_Sever():
                     self.conn.sendall(send_data.encode("utf_8"))
                 except Exception as e:
                     self.sendError("data read failed: %s" %e)
-            elif data[0] == b"get_delay":
+            elif data[0] == "get_delay":
                 try:
                     send_data= self.get_delay_time()
+
+                    self.conn.sendall(send_data.encode("utf_8"))
+                except Exception as e:
+                    self.sendError("data read failed: %s" %e)
+
+            elif data[0] == "pass_command":
+                try:
+                    send_data= self.pass_command(data[1])
 
                     self.conn.sendall(send_data.encode("utf_8"))
                 except Exception as e:
@@ -104,6 +123,8 @@ class Pulse_G_Sever():
     def sendError(self, errorMessage = "Unknown request"):
         self.conn.sendall(errorMessage.encode("utf_8"))
     
+    def reset_output(self):
+        self.device.serial.reset_output_buffer()
     def sendAck(self):
         sendData = b"1"
         self.conn.sendall(sendData)
--- src/start_sever.py
+++ src/start_sever.py
@@ -3,7 +3,6 @@ Created on 24 Mar 2025
 
 @author: wvx67826
 '''
-from src.pulse_generate_sever import Pulse_G_Sever
-
+from pulse_generator_sever import Pulse_G_Sever
 pgs = Pulse_G_Sever(port = 16888)
 pgs.start()
